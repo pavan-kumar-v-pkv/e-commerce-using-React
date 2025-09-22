bb79c5ed20b31da0de5049d2add325c53341e2be
pkv-woodstock <pkvstarscream@gmail.com>
2025-09-22 16:03:47 +0530

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { products } from "../../starting-code/data/products.js";
 import "./HomePage.css";
 
 export function HomePage() {
+  fetch('http://localhost:3001/api/products')
+  .then((response) => {
+    response.json()
+  }).then((data) => {
+    console.log(data);
+  });
+  
   return (
     <>
       <title>Ecommerce Project</title>
