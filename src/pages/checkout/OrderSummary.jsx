fb80828023d3a5036623c3489940e2bebe0d71be
pkv-woodstock <pkvstarscream@gmail.com>
2025-09-22 19:43:58 +0530

    add update delivery option functionality

--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
 
-export function OrderSummary({ cart, deliveryOptions}) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
@@ -44,7 +44,7 @@ export function OrderSummary({ cart, deliveryOptions}) {
                   </div>
                 </div>
 
-                <DeliveryOptions deliveryOptions={deliveryOptions} item={item} />
+                <DeliveryOptions deliveryOptions={deliveryOptions} item={item} loadCart={loadCart} />
               </div>
             </div>
           );
