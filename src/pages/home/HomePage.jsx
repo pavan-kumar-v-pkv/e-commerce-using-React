77abfe608d4d17960635a2f0bc80448a996088e5
pkv-woodstock <pkvstarscream@gmail.com>
2025-09-22 18:57:30 +0530

    covert home and checkout to async

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,17 +6,19 @@ import "./HomePage.css";
 
 export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    axios.get('/api/products')
-    .then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
+    getHomeData();
   }, []);
 
   return (
     <>
       <title>Ecommerce Project</title>
-      <Header cart={cart}/>
+      <Header cart={cart} />
 
       <div className="home-page">
         <ProductsGrid products={products} />
